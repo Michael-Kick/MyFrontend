4780a89c66266cd660f6af1cc9b024f7c564ac8d
Michael Kick <michael-kick@gmx.de>
2024-12-26 14:30:06 +0100

    added tailwind

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 function HeroSection() {
     return (
         <section>
-            <h1> Hello its ya boy michael </h1>
+            <h1 className="text-6xl"> Hello its ya boy michael </h1>
             <p>Lorem ipsum dolor sit amet, consetetur sadipscing
                 elitr, sed diam nonumy eirmod tempor invidunt ut
                 labore et dolore magna aliquyam erat, sed diam
