7249876145dc03e0ec0323d7a3175ecdd017d749
micha <michael-kick@gmx.de>
2025-01-13 13:14:05 +0100

    Added SkillTag styling

--- src/app/_components/ProjectCard.tsx
+++ src/app/_components/ProjectCard.tsx
@@ -3,12 +3,14 @@ import SkillTag from "./SkillTag";
 
 const ProjectCard = () => {
     console.log("(1)")
-    const projSkills = [1,2,3,4,5,6,7,8]
+    const projSkills: number[] = [1,2,3,4,5,6,7,8]
     return (
         <div>
-            <div> Project Overview</div>
+            <h3> Project Name</h3>
+            <div>COMPANY ICON</div>
+            <div>Company name and Department</div>
             <div>
-                {projSkills.map((skillNum) =>
+                {projSkills.map((skillNum: number) =>
                     <SkillTag skillKey={skillNum}/>
                 )}
             </div>
--- src/app/_components/SkillTag.tsx
+++ src/app/_components/SkillTag.tsx
@@ -1,6 +1,6 @@
 "use client";
 import React from 'react';
-import { FaVuejs, FaReact, FaPython, FaJava} from "react-icons/fa";
+import {FaJava, FaPython, FaReact, FaVuejs} from "react-icons/fa";
 import {TbBrandCSharp, TbBrandXamarin} from "react-icons/tb";
 import {BsFiletypeSql} from "react-icons/bs";
 import {BiLogoTypescript} from "react-icons/bi";
@@ -9,24 +9,28 @@ type SkillTagProps = {
     skillKey: number;
 }
 const mySkills = [
-    {key: 1, name: 'Vue.js', icon: <FaVuejs/>},
-    {key: 2, name: 'TypeScript', icon: <BiLogoTypescript/>},
-    {key: 3, name: 'SQL', icon: <BsFiletypeSql/>},
-    {key: 4, name: 'React.js', icon: <FaReact/>},
-    {key: 5, name: 'Python', icon: <FaPython/>},
-    {key: 6, name: 'C#', icon: <TbBrandCSharp/>},
-    {key: 7, name: 'Java', icon: <FaJava/>},
-    {key: 8, name: 'Xamarin.Forms', icon: <TbBrandXamarin/>},
+    {key: 1, name: 'Vue.js', icon: <FaVuejs/>, textColor:'#31475e', backgroundColor:'#3fb984'},
+    {key: 2, name: 'TypeScript', icon: <BiLogoTypescript/>, textColor:'#ffffff', backgroundColor:'#2d79c7'},
+    {key: 3, name: 'SQL', icon: <BsFiletypeSql/>, textColor:'#ffffff', backgroundColor:'#087993'},
+    {key: 4, name: 'React.js', icon: <FaReact/>, textColor:'#00d8ff', backgroundColor:'#222222'},
+    {key: 5, name: 'Python', icon: <FaPython/>, textColor:'#ffffff', backgroundColor:'#646464'},
+    {key: 6, name: 'C-Sharp', icon: <TbBrandCSharp/>, textColor:'#ffffff', backgroundColor:'#690081'},
+    {key: 7, name: 'Java', icon: <FaJava/>, textColor:'#f58620', backgroundColor:'#547c99'},
+    {key: 8, name: 'Xamarin.Forms', icon: <TbBrandXamarin/>, textColor:'#ffffff', backgroundColor:'#3a9bdc'},
 ]
 
-function SkillTag(props:SkillTagProps){
+
+function SkillTag(props: SkillTagProps) {
 
     // const [count, setCount] = useState(0);
     const skill = mySkills.find((el) => el.key == props.skillKey);
     return (
-        <div>
-            <p>{skill.name}</p>
-            {skill.icon}
+        <div style={{backgroundColor:skill?.backgroundColor}}
+             className="border border-contrast rounded-2xl flex justify-center w-fit bg-">
+            <div className="flex h-auto items-center mt-0.5 mb-0.5 ms-3 me-1">
+                {skill?.icon}
+            </div>
+            <h1 style={{color:skill?.textColor}} className="mt-0.5 mb-0.5 ms-1 me-3">{skill?.name}</h1>
         </div>
     );
 }
