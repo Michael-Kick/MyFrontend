b6bddffbf2119a52288f3d7d8885c1609ca59bf7
Micha <michael-kick@gmx.de>
2025-02-03 09:06:15 +0100

    Customized Input
    - filled forms can now be submitted!

--- src/app/_components/CustomizedInput.tsx
+++ src/app/_components/CustomizedInput.tsx
@@ -4,17 +4,17 @@ interface CustomizedInputProps {
     labelText:string;
     placeholder:string;
     type:string;
-    onChange?: () => {};
+    setValue: (value: string) => void;
 }
 
-function CustomizedInput({labelText, placeholder, type, onChange}:CustomizedInputProps) {
+function CustomizedInput({labelText, placeholder, type, setValue}:CustomizedInputProps) {
     return (
         <div className='mb-2 mt-2'>
             <label className="block mb-2 text-text">{labelText}</label>
             <input type={type}
                    className="bg-contrastDark border border-contrastDark text-text rounded-lg focus:ring-primary focus:border-primary block w-full p-2.5 "
                    placeholder={placeholder}
-                   onChange={onChange}
+                   onChange={(event) => setValue(event.target.value)}
                    />
         </div>
     );
--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React from 'react';
+import React, { useState } from 'react';
 import Headline from "../_components/Headline";
 import CustomizedInput from "../_components/CustomizedInput";
 import CustomizedButton from '../_components/CustomizedButton';
@@ -9,63 +9,72 @@ interface inputProps {
     label: string;
     placeholder:string;
     type:string;
+    onChange: (val:string) => void
 }
 
-const inputProps = [
-    {
-        label: "First Name",
-        placeholder: "Max",
-        type: "text",
-    },
-    {
-        label: "Last Name",
-        placeholder: "Mustermann",
-        type: "text",
-    },
-    {
-        label: "Company",
-        placeholder: "My Garage Business",
-        type: "text",
-    },
-    {
-        label: "Phone Number",
-        placeholder: "123-45-678",
-        type: "tel",
-    },
-    {
-        label: "E-Mail address",
-        placeholder: "max.mustermann@mygarage.com",
-        type: "email",
-    },
-]
-
-
-
 const Contact = () => {
-
-    const onNameChange = () => {
-        
-    }
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
+    const [company, setCompany] = useState('');
+    const [phoneNumber, setPhoneNumber] = useState('');
+    const [email, setEmail] = useState('');
+    const [message, setMessage] = useState('')
 
     const submitClicked = () => {
-        alert("submit")
+        alert("submitted: " + firstName + lastName+ company + email +phoneNumber + message)
     }
 
+
+    const inputProps = [
+        {
+            label: "First Name",
+            placeholder: "Max",
+            type: "text",
+            onChange: setFirstName
+        },
+        {
+            label: "Last Name",
+            placeholder: "Mustermann",
+            type: "text",
+            onChange: setLastName
+        },
+        {
+            label: "Company",
+            placeholder: "My Garage Business",
+            type: "text",
+            onChange: setCompany
+        },
+        {
+            label: "Phone Number",
+            placeholder: "123-45-678",
+            type: "tel",
+            onChange: setPhoneNumber
+        },
+        {
+            label: "E-Mail address",
+            placeholder: "max.mustermann@mygarage.com",
+            type: "email",
+            onChange: setEmail
+        },
+    ]
 
     return (
         <div>
             <Headline text="Contact Me"/>
-            {inputProps.map((input:inputProps,id ) =>
+            {inputProps.map((input:inputProps,id: number) =>
                     <CustomizedInput
                         labelText={input.label}
                         placeholder={input.placeholder}
                         type={input.type}
-                        key={id} />
+                        setValue={input.onChange}
+                        key={id} 
+                        />
                 )
             }
             <div className="mb-2 mt-2">
               <label className="block mb-2 text-text">Your message</label>
-              <textarea id="message" rows={3} className="block p-2.5 w-full text-text bg-contrastDark border border-contrastDark rounded-lg focus:ring-primary focus:border-primary" placeholder="Leave a comment..."></textarea>
+              <textarea id="message" rows={3} onChange={(e) => setMessage(e.target.value)} 
+              className="block p-2.5 w-full text-text bg-contrastDark border border-contrastDark rounded-lg focus:ring-primary focus:border-primary" placeholder="Leave a comment..."></textarea>
           </div>
           <CustomizedButton text='Submit' onClick={submitClicked}/>
         </div>
